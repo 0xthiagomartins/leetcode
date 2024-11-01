8f3d3f0de4881f7a18c832a4fb1fca3a082aef0c
Thiago Martins <thiago.fmartins@outlook.com>
2024-10-31 22:41:09 -0300

    importing HashMap globally

--- codes/two-sum/main.rs
+++ codes/two-sum/main.rs
@@ -1,43 +1,43 @@
+use std::collections::HashMap;
+
 struct TwoSum;
 
 impl TwoSum {
-    fn brute_force(&self, nums: &[i32], target: i32) -> Vec<usize> {
-        for (i1, &num1) in nums.iter().enumerate() {
-            for (i2, &num2) in nums.iter().enumerate() {
+    fn brute_force(&self, nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for (i1, num1) in nums.iter().enumerate() {
+            for (i2, num2) in nums.iter().enumerate() {
                 if num1 + num2 == target && i1 != i2 {
-                    return vec![i1, i2];
+                    return vec![i1 as i32, i2 as i32];
                 }
             }
         }
         vec![]
     }
 
-    fn two_pass_hash_table(&self, nums: &[i32], target: i32) -> Vec<usize> {
-        use std::collections::HashMap;
+    fn two_pass_hash_table(&self, nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut lookup = HashMap::new();
-        for (i, &num) in nums.iter().enumerate() {
-            lookup.insert(num, i);
+        for (i, num) in nums.iter().enumerate() {
+            lookup.insert(*num, i);
         }
-        for (i, &num) in nums.iter().enumerate() {
+        for (i, num) in nums.iter().enumerate() {
             let diff = target - num;
             if let Some(&j) = lookup.get(&diff) {
                 if i != j {
-                    return vec![i, j];
+                    return vec![i as i32, j as i32];
                 }
             }
         }
         vec![]
     }
 
-    fn one_pass_hash_table(&self, nums: &[i32], target: i32) -> Vec<usize> {
-        use std::collections::HashMap;
+    fn one_pass_hash_table(&self, nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut lookup = HashMap::new();
-        for (i, &num) in nums.iter().enumerate() {
+        for (i, num) in nums.iter().enumerate() {
             let diff = target - num;
             if let Some(&j) = lookup.get(&diff) {
-                return vec![j, i];
+                return vec![j as i32, i as i32];
             }
-            lookup.insert(num, i);
+            lookup.insert(*num, i);
         }
         vec![]
     }
@@ -46,7 +46,7 @@ impl TwoSum {
 fn main() {
     let solution = TwoSum;
 
-    let approaches: Vec<fn(&TwoSum, &[i32], i32) -> Vec<usize>> = vec![
+    let approaches: Vec<fn(&TwoSum, Vec<i32>, i32) -> Vec<i32>> = vec![
         TwoSum::brute_force,
         TwoSum::two_pass_hash_table,
         TwoSum::one_pass_hash_table,
@@ -65,19 +65,19 @@ fn main() {
         // Test case 1
         let nums1 = vec![2, 7, 11, 15];
         let target1 = 9;
-        assert_eq!(approach(&solution, &nums1, target1), vec![0, 1]);
+        assert_eq!(approach(&solution, nums1, target1), vec![0, 1]);
         println!("Test case 1 passed");
 
         // Test case 2
         let nums2 = vec![3, 2, 4];
         let target2 = 6;
-        assert_eq!(approach(&solution, &nums2, target2), vec![1, 2]);
+        assert_eq!(approach(&solution, nums2, target2), vec![1, 2]);
         println!("Test case 2 passed");
 
         // Test case 3
         let nums3 = vec![3, 3];
         let target3 = 6;
-        assert_eq!(approach(&solution, &nums3, target3), vec![0, 1]);
+        assert_eq!(approach(&solution, nums3, target3), vec![0, 1]);
         println!("Test case 3 passed\n");
     }
 }
